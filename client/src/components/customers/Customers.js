b9d52f9ddffa79a88e8ebe2026b784d2d2760cdb
Sagnik Haldar <sagnikh2@gmail.com>
2021-12-31 00:46:55 +0530

    set env

--- client/src/components/customers/Customers.js
+++ client/src/components/customers/Customers.js
@@ -2,28 +2,30 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import CustomerForm from "./CustomerForm";
 import CustomerList from "./CustomerList";
+import dotenv from "dotenv";
+dotenv.config();
 
 function Customers() {
-  const [customers, setCustomers] = useState([]);
+	const [customers, setCustomers] = useState([]);
 
-  async function getCustomers() {
-    // const customersRes = await axios.get("http://localhost:5000/customer/");
-    const customersRes = await axios.get(
-      "https://mern-auth-template-tutorial.herokuapp.com/customer/"
-    );
-    setCustomers(customersRes.data);
-  }
+	async function getCustomers() {
+		// const customersRes = await axios.get("http://localhost:5000/customer/");
+		const customersRes = await axios.get(
+			`${process.env.REACT_APP_SERVER_URL}/customer/`
+		);
+		setCustomers(customersRes.data);
+	}
 
-  useEffect(() => {
-    getCustomers();
-  }, []);
+	useEffect(() => {
+		getCustomers();
+	}, []);
 
-  return (
-    <div>
-      <CustomerForm getCustomers={getCustomers} />
-      <CustomerList customers={customers} />
-    </div>
-  );
+	return (
+		<div>
+			<CustomerForm getCustomers={getCustomers} />
+			<CustomerList customers={customers} />
+		</div>
+	);
 }
 
 export default Customers;
